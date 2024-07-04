50dde1a4af0f6ca3e56718b5857b2b1e81434416
aassalax <aassali.dev@gmail.com>
2024-07-04 23:42:24 +0200

    redirect to auth component when 401

--- src/app/interceptors/auth.interceptor.ts
+++ src/app/interceptors/auth.interceptor.ts
@@ -15,21 +15,22 @@ export class AuthInterceptor implements HttpInterceptor {
 
   private handleAuthError(err: HttpErrorResponse): Observable<any> {
     if (err.status === 401 || err.status === 403) {
-      this.router.navigateByUrl(`/login`).then(r => r);
+      this.router.navigateByUrl(`/auth`).then(r => r);
       return of(err.message);
     }
     return throwError(() => new Error('An error occurred !'));
   }
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
-    console.log("INTERCEPTOR:",req);
     const authToken = this.auth.getToken();
     if(authToken){
       const authReq = req.clone({
         headers: req.headers.set('Authorization', "Bearer "+ authToken)
       });
-      return next.handle(authReq).pipe(catchError(x=> this.handleAuthError(x)));
+      return next.handle(authReq)
+        .pipe(catchError(x=> this.handleAuthError(x)));
     }
-    return next.handle(req);
+    return next.handle(req)
+      .pipe(catchError(x=> this.handleAuthError(x)));
   }
 }
